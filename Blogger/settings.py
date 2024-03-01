c3225be0e9aa6744694188ae6a49ca1d7ab3be48
Deepak Kumar <deepakt90su30@gmail.com>
2024-03-02 01:37:25 +0530

    commit

--- Blogger/settings.py
+++ Blogger/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8*)&+px7*j&%5ociz$6gthw^*hffr%s0wf1y4s^!+db$-vej%q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','afa61242.ngrok.io']
 
 
 # Application definition
